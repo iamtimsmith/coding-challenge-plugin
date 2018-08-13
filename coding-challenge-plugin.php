0f4bf2024bd4c85b254820d7419ef6c741b973b8
Tim Smith <tgsmith489@gmail.com>
2018-08-12 21:26:29 -0500

    Added form submission to settings page

--- coding-challenge-plugin.php
+++ coding-challenge-plugin.php
@@ -40,11 +40,33 @@ add_action('admin_menu', 'coding_challenge_menu_item');
 /**
  * Add page for plugin settings
  */
-function coding_challenge_settings() { ?>
+function coding_challenge_settings() { 
+  
+  if(array_key_exists('submit_cc_settings', $_POST)) {
+    update_option('cc_site_one', $_POST['site-one']);
+    update_option('cc_site_two', $_POST['site-two']);
+    update_option('cc_site_three', $_POST['site-three']);
+    update_option('cc_site_four', $_POST['site-four']);
+    update_option('cc_site_five', $_POST['site-five']);
+
+    echo "<div id='setting-update' class='updated settings-error notice is-dismissible'><strong>Settings have been saved.</strong></div>";
+  }
+  
+  // Get values
+  $site1 = get_option('cc_site_one');
+  $site2 = get_option('cc_site_two');
+  $site3 = get_option('cc_site_three');
+  $site4 = get_option('cc_site_four');
+  $site5 = get_option('cc_site_five');
+  
+  
+  ?>
+
   <div class="wrap">
     <h2>Coding Challenge Settings</h2>
-    <form action="">
-      <div style="margin-bottom:20px"><label for="site-one">Site 1 URL: <input type="text" name="site-one" style="width:500px" required></label></div>
+    <form action="" method="post">
+      <p style="color:#888; font-style:italic">URLs should be entered with no trailing slash. Sites must be Wordpress sites to function properly.</p>
+      <div style="margin-bottom:20px"><label for="site-one">Site 1 URL: <input type="text" name="site-one" style="width:500px" value="<?php echo $site1; ?>"></label></div>
       <div style="margin-bottom:20px"><label for="site-two">Site 2 URL: <input type="text" name="site-two" style="width:500px"></label></div>
       <div style="margin-bottom:20px"><label for="site-three">Site 3 URL: <input type="text" name="site-three" style="width:500px"></label></div>
       <div style="margin-bottom:20px"><label for="site-four">Site 4 URL: <input type="text" name="site-four" style="width:500px"></label></div>
@@ -52,7 +74,7 @@ function coding_challenge_settings() { ?>
       <p></p>
       <hr>
       <p></p>
-      <button class="button button-primary">Save Settings</button>
+      <input type="submit" name="submit_cc_settings" class="button button-primary" value="Save Settings"/>
     </form>
   </div>
 
