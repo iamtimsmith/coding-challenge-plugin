c6ef7b587d8c3d701a9dba4d52af3e9740d6085d
Tim Smith <tgsmith489@gmail.com>
2018-08-12 22:15:04 -0500

    Modified options pulled to remove trailing slashes and prevent errors, added sorting to put newest items first in array, limit array to 10 items

--- coding-challenge-plugin.php
+++ coding-challenge-plugin.php
@@ -21,9 +21,50 @@
 function coding_challenge_dashboard() { ?>
   <div class="wrap">
     <h2>Imported Posts</h2>
+    <table>
+
+    <?php
+    $url_for_posts = [
+      "http://" . parse_url( get_option('cc_site_one') )['host'],
+      "http://" . parse_url( get_option('cc_site_two') )['host'],
+      "http://" . parse_url( get_option('cc_site_three') )['host'],
+      "http://" . parse_url( get_option('cc_site_four') )['host'],
+      "http://" . parse_url( get_option('cc_site_five') )['host']
+    ];
+    $list_of_posts = [];
+
+    foreach ($url_for_posts as $url) {
+      $types = ['posts', 'pages'];
+      foreach ($types as $type) {
+        $body = wp_remote_retrieve_body( wp_remote_get("$url/wp-json/wp/v2/$type") );
+        $results = json_decode($body);
+        foreach ($results as $result) {
+          array_push($list_of_posts, $result);
+        }
+      }
+    }
+    
+    // Sort by date
+    function sort_posts_by_date($a, $b) {
+      if( strtotime($a->modified) == strtotime($b->modified) ){ return 0;}
+      return ( strtotime($a->modified) > strtotime($b->modified)) ? -1 : 1;
+    }
+    usort($list_of_posts, 'sort_posts_by_date');
+
+    foreach (array_slice($list_of_posts, 0, 10) as $post) {
+
+      $render = "<tr>";
+      $render .= "<td>" . $post->title->rendered ."</td>";
+      $render .= "<td>" . parse_url($post->guid->rendered)['host'] ."</td>";
+      $render .= "<td>" . $post->modified . "</td>";
+      $render .= "<td><button class='button button-primary'>Add</button></td>";
+      $render .= "</tr>";
+      echo $render;
+    }
 
 
-
+    ?>
+    </table>
   </div>
 
 <?php }
@@ -43,21 +84,21 @@ add_action('admin_menu', 'coding_challenge_menu_item');
 function coding_challenge_settings() { 
   
   if(array_key_exists('submit_cc_settings', $_POST)) {
-    update_option('cc_site_one', $_POST['site-one']);
-    update_option('cc_site_two', $_POST['site-two']);
-    update_option('cc_site_three', $_POST['site-three']);
-    update_option('cc_site_four', $_POST['site-four']);
-    update_option('cc_site_five', $_POST['site-five']);
+    update_option('cc_site_one', $_POST['site-one'] );
+    update_option('cc_site_two', $_POST['site-two'] );
+    update_option('cc_site_three', $_POST['site-three'] );
+    update_option('cc_site_four', $_POST['site-four'] );
+    update_option('cc_site_five', $_POST['site-five'] );
 
     echo "<div id='setting-update' class='updated settings-error notice is-dismissible'><strong>Settings have been saved.</strong></div>";
   }
   
   // Get values
-  $site1 = get_option('cc_site_one');
-  $site2 = get_option('cc_site_two');
-  $site3 = get_option('cc_site_three');
-  $site4 = get_option('cc_site_four');
-  $site5 = get_option('cc_site_five');
+  $site1 = get_option('cc_site_one', '');
+  $site2 = get_option('cc_site_two', '');
+  $site3 = get_option('cc_site_three', '');
+  $site4 = get_option('cc_site_four', '');
+  $site5 = get_option('cc_site_five', '');
   
   
   ?>
@@ -67,10 +108,10 @@ function coding_challenge_settings() {
     <form action="" method="post">
       <p style="color:#888; font-style:italic">URLs should be entered with no trailing slash. Sites must be Wordpress sites to function properly.</p>
       <div style="margin-bottom:20px"><label for="site-one">Site 1 URL: <input type="text" name="site-one" style="width:500px" value="<?php echo $site1; ?>"></label></div>
-      <div style="margin-bottom:20px"><label for="site-two">Site 2 URL: <input type="text" name="site-two" style="width:500px"></label></div>
-      <div style="margin-bottom:20px"><label for="site-three">Site 3 URL: <input type="text" name="site-three" style="width:500px"></label></div>
-      <div style="margin-bottom:20px"><label for="site-four">Site 4 URL: <input type="text" name="site-four" style="width:500px"></label></div>
-      <div style="margin-bottom:20px"><label for="site-five">Site 5 URL: <input type="text" name="site-five" style="width:500px"></label></div>
+      <div style="margin-bottom:20px"><label for="site-two">Site 2 URL: <input type="text" name="site-two" style="width:500px"value="<?php echo $site2; ?>"></label></div>
+      <div style="margin-bottom:20px"><label for="site-three">Site 3 URL: <input type="text" name="site-three" style="width:500px"value="<?php echo $site3; ?>"></label></div>
+      <div style="margin-bottom:20px"><label for="site-four">Site 4 URL: <input type="text" name="site-four" style="width:500px"value="<?php echo $site4; ?>"></label></div>
+      <div style="margin-bottom:20px"><label for="site-five">Site 5 URL: <input type="text" name="site-five" style="width:500px"value="<?php echo $site5; ?>"></label></div>
       <p></p>
       <hr>
       <p></p>
